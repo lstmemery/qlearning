46141c928f73bbe5018ae39f081bbbd6044ed036
Matthew Emery <m.emery@alumni.ubc.ca>
2017-05-26 12:53:38 -0700

    Make transition matrix fails

--- qlearning_numpy.py
+++ qlearning_numpy.py
@@ -1,9 +1,18 @@
 import numpy as np
 
 
-reward_grid = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 1],
+state_grid = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 1],
                         [0, 0, 0, 0, 0, 0, 0, 0, 0],
                         [0, 0, 0, 0, 0, 0, 0, 0, 0],
                         [-1, -1, -1, -1, -1, -1, -1, -1, 0],
                         [0, 0, 0, 0, 0, 0, 0, 0, 0],
                         [0, 0, 0, 0, 0, 0, 0, 0, 0]])
+
+
+
+action_grid = np.zeros((6, 9))
+
+def move_grid()
+
+if __name__ == '__main__':
+    print(action_grid)
--- src/test/test_qlearning_numpy.py
+++ src/test/test_qlearning_numpy.py
@@ -1,4 +1,13 @@
 from qlearning_numpy import *
 
 def test_q_learning_size():
-    assert reward_grid.shape == (6, 9)
+    assert state_grid.shape == (6, 9)
+
+def test_make_transition_matrix():
+    input_matrix = np.array([-1, 1],
+                            [0, 0])
+    output_matrix = np.array([[-1, -1, -1, -1],
+                              [-1, -1, -1,  0],
+                              [-1, -1, -1, -1],
+                              [-1,  0,  1, -1]])
+    assert make_transition_matrix(input_matrix) == output_matrix
