3496c092535ac4942b4a2885a8f1b44c14b976d4
Matthew Emery <m.emery@alumni.ubc.ca>
2017-05-26 08:34:12 -0700

    test gridworld object

---
+++ src/qlearning.py
@@ -0,0 +1,6 @@
+# Implement Q-learning algorithm 1 for solving the Gridworld (Section 4).
+# We recommend implementing a Q(s, a) function as a simple 2-dimensional
+# array, since there are only 46 states and 4 actions from each state. Initialize
+# Q(s, a) values with 0 for all pairs (s, a). Set discount factor γ = 0.95.
+# Choose the values of all other necessary parameters as you see fit, a little
+# exploration of a parameters space can go a long way.
---
+++ src/test/__init__.py
@@ -0,0 +1,5 @@
+import pytest
+
+def test_grid_world():
+    grid_world = GridWorld()
+    assert grid_world.size == 46
