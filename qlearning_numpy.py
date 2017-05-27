cb85edf0899fac54ab9dcf3e8dc530260317faf9
Matthew Emery <m.emery@alumni.ubc.ca>
2017-05-27 09:57:30 -0700

    make_transition_matrix does not work

--- qlearning_numpy.py
+++ qlearning_numpy.py
@@ -12,7 +12,27 @@ state_grid = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 1],
 
 action_grid = np.zeros((6, 9))
 
-def move_grid()
+def make_transition_matrix(matrix):
+    transition_matrix = np.zeros((matrix.size, matrix.size)) - 1
+
+    rows, columns = matrix.shape
+
+    for row in range(0, rows):
+        for column in range(0, columns):
+            if row > 0 and matrix[row - 1, column] >= 0:
+                transition_matrix[column * rows + row, column * rows + row - 1] = matrix[row - 1, column]
+
+            if row < rows and matrix[row + 1, column] >= 0:
+                transition_matrix[column * rows + row, column * rows + row + 1] = matrix[row + 1, column]
+
+            if column > 0 and matrix[row, column - 1] >= 0:
+                transition_matrix[column * rows + row, (column - 1) * rows + row] = matrix[row, column - 1]
+
+            if column > 0 and matrix[row, column - 1] >= 0:
+                transition_matrix[column * rows + row, (column - 1) * rows + row] = matrix[row, column - 1]
+
+    print(transition_matrix)
+    return transition_matrix
 
 if __name__ == '__main__':
     print(action_grid)
--- src/test/test_qlearning_numpy.py
+++ src/test/test_qlearning_numpy.py
@@ -1,13 +1,13 @@
 from qlearning_numpy import *
 
 def test_q_learning_size():
-    assert state_grid.shape == (6, 9)
+    assert state_grid.shape == (42, 4)
 
 def test_make_transition_matrix():
-    input_matrix = np.array([-1, 1],
-                            [0, 0])
+    input_matrix = np.array([[-1, 1],
+                             [0, 0]])
     output_matrix = np.array([[-1, -1, -1, -1],
                               [-1, -1, -1,  0],
                               [-1, -1, -1, -1],
                               [-1,  0,  1, -1]])
-    assert make_transition_matrix(input_matrix) == output_matrix
+    assert make_transition_matrix(input_matrix).all() == output_matrix
