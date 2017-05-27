ba276b12ead5a7ce515c490d7ea56e27b90f8e59
Matthew Emery <m.emery@alumni.ubc.ca>
2017-05-27 10:32:02 -0700

    Test index_1d fails

--- qlearning_numpy.py
+++ qlearning_numpy.py
@@ -1,19 +1,21 @@
 import numpy as np
 
-
 state_grid = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 1],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [-1, -1, -1, -1, -1, -1, -1, -1, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0]])
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [-1, -1, -1, -1, -1, -1, -1, -1, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0]])
+
+#TODO: Don;t forget about the transition at 1000 iterations
 
 
 
-action_grid = np.zeros((6, 9))
+
+
+
 
 def make_transition_matrix(matrix):
-
     padded_matrix = np.pad(matrix, pad_width=1, mode="constant", constant_values=-1)
 
     up = np.roll(padded_matrix, shift=1, axis=0)[1:-1, 1:-1].flatten()
@@ -24,6 +26,17 @@ def make_transition_matrix(matrix):
     transition_matrix = np.stack((up, right, down, left), axis=0).T
 
     return transition_matrix
+
+def index_1d(row, col):
+    pass
+
+def qlearning(grid, episodes):
+    r_matrix = make_transition_matrix(state_grid)
+    q_matrix = np.zeros_like(r_matrix)
+    for episode in episodes:
+        pass
+
+
 
 if __name__ == '__main__':
     print(make_transition_matrix(state_grid))
--- src/test/test_qlearning_numpy.py
+++ src/test/test_qlearning_numpy.py
@@ -3,11 +3,5 @@ from qlearning_numpy import *
 def test_q_learning_size():
     assert make_transition_matrix(state_grid).shape == (46, 4)
 
-def test_make_transition_matrix():
-    input_matrix = np.array([[-1, 1],
-                             [0, 0]])
-    output_matrix = np.array([[-1, -1, -1, -1],
-                              [-1, -1, -1,  0],
-                              [-1, -1, -1, -1],
-                              [-1,  0,  1, -1]])
-    assert make_transition_matrix(input_matrix).all() == output_matrix
+def test_index_1d():
+    assert index_1d(5, 3) == 48
