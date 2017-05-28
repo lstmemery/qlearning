a90f993843a55bc19738b295e6edeff9336868f5
Matthew Emery <m.emery@alumni.ubc.ca>
2017-05-28 12:34:09 -0700

    Fixed state bug

--- src/async_qlearning.py
+++ src/async_qlearning.py
@@ -1,11 +1,16 @@
-from multiprocessing import Process, Queue, Value, Array, Manager
+from multiprocessing import Process, Value, Array, Manager
 import numpy as np
 import src.qlearning_numpy as ql
-import time
-from random import random
 import ctypes
 
-def qlearning_worker(r_matrix, epsilon, alpha, gamma, async_update, T, Tmax, q, global_q_matrix):
+
+def update_delta_q(q, state, action, gamma, reward, next_state):
+    max_next_step = max(q[next_state, :])
+    next_q = q[state, action] + (reward + gamma * max_next_step - q[state, action])
+    return next_q
+
+
+def qlearning_worker(r_matrix, epsilon, gamma, async_update, T, Tmax, q, global_q_matrix):
     # Initialize thread step count t <- 1 (Not sure why this starts at 1)
     t = 1
     delta_q_matrix = np.zeros_like(r_matrix).astype(float)
@@ -23,7 +28,7 @@ def qlearning_worker(r_matrix, epsilon, alpha, gamma, async_update, T, Tmax, q, 
         # Take action a, observe r, s'
         reward = ql.peek_reward(r_matrix, state, action)
         next_state = ql.peek_next_state(r_matrix, state, action)
-        updated_q = ql.update_q(delta_q_matrix, state, action, alpha, gamma, reward, next_state)
+        updated_q = update_delta_q(delta_q_matrix, state, action, gamma, reward, next_state)
         # Accumulate updates:
         delta_q_matrix[state, action] = updated_q
         # s <- s'
@@ -34,41 +39,31 @@ def qlearning_worker(r_matrix, epsilon, alpha, gamma, async_update, T, Tmax, q, 
         T.value += 1
         print(T.value)
         # if t % I_AsyncUpdate == 0 or s is terminal then
-        if t % async_update == 0 or state != ql.index_1d(0, 8):
+        if t % async_update == 0 or state == ql.index_1d(0, 8):
             # Perform async update
             send_local_q(q, delta_q_matrix)
             # clear updates \DeltaQ(s', a')
             delta_q_matrix = np.zeros_like(r_matrix).astype(float)
-            if state != ql.index_1d(0, 8):
+            if state == ql.index_1d(0, 8):
                 state = start_state
 
-    print("returning")
-
-
-def receive_global_q(parent_conn, lock):
-    try:
-        lock.acquire()
-        global_q = parent_conn.recv()
-        return global_q
-    finally:
-        lock.release()
+    print("worker returning")
 
 
 def send_local_q(q, delta_q_matrix):
     q.put(delta_q_matrix)
 
 
-def acculmulate_q(q, global_q_matrix, T, Tmax):
-    while T.value <= Tmax:
-        if not q.empty():
-            local_q_matrix = q.get()
-            with global_q_matrix.get_lock():
-                q_np = to_numpy_array(global_q_matrix, local_q_matrix.shape)
-                q_np += local_q_matrix
-                global_q_matrix = to_mp_array(q_np)
+def acculmulate_q(q, global_q_matrix, T, Tmax, alpha):
+    q_np = None
+    while T.value <= Tmax or not q.empty():
+        local_q_matrix = q.get()
+        with global_q_matrix.get_lock():
+            q_np = to_numpy_array(global_q_matrix, local_q_matrix.shape)
+            q_np += alpha * local_q_matrix
+            global_q_matrix = to_mp_array(q_np)
 
-    print(q.empty())
-    print("consumer returning")
+    q.put(q_np)
     return
 
 
@@ -96,18 +91,16 @@ def async_manager(processes, epsilon, alpha, gamma, async_update, Tmax):
     q = manager.Queue()
 
     consumer = Process(target=acculmulate_q, args=(q, global_q_matrix,
-                                                   T, Tmax))
+                                                   T, Tmax, alpha))
 
-
-    producer1 = Process(target=qlearning_worker, args=(r_matrix, epsilon,
-                                                      alpha, gamma,
+    producer1 = Process(target=qlearning_worker, args=(r_matrix, epsilon, gamma,
                                                       async_update, T,
                                                       Tmax, q, global_q_matrix))
 
     producer1.start()
 
     producer2 = Process(target=qlearning_worker, args=(r_matrix, epsilon,
-                                                      alpha, gamma,
+                                                       gamma,
                                                       async_update, T,
                                                       Tmax, q, global_q_matrix))
 
@@ -119,7 +112,9 @@ def async_manager(processes, epsilon, alpha, gamma, async_update, Tmax):
     producer1.join()
     producer2.join()
 
-    return to_numpy_array(global_q_matrix, r_matrix.shape)
+    q_np = q.get()
+
+    return q_np
 
 if __name__ == '__main__':
     print(async_manager(processes=2,
@@ -127,4 +122,4 @@ if __name__ == '__main__':
                   alpha=0.1,
                   gamma=0.95,
                   async_update=4,
-                  Tmax=100))
+                  Tmax=10000))
